a6937658a53c7a657bdf3791f012c914333310f0
Jasper Bernales <jasper@dnamicro.com>
2017-07-20 16:26:47 +0800

    Added more code and notes

--- generators.js
+++ generators.js
@@ -7,49 +7,52 @@ Promise.promisifyAll(request);
 //   const one = yield 1;
 //   const two = yield 2;
 //   const three = yield 3;
-//   console.log(one, two, three); // 3, 4, 5
+//   console.log(one, two, three); // 4, 5, 6
 // };
 //
 // const gen = myGen(); //get the generator ready to run
 // console.log(gen.next()); //{value:1, done: false}
-// console.log(gen.next(3)); //{value:2, done: false}
-// console.log(gen.next(4)); //{value:3, done: false}
-// console.log(gen.next(5)); //{value:undefined, done: true}
+// console.log(gen.next(4)); //{value:2, done: false}
+// console.log(gen.next(5)); //{value:3, done: false}
+// console.log(gen.next(6)); //{value:undefined, done: true}
 // console.log(gen.next()); //{value:undefined, done: true}
 
 // code taken from DOCS
-// function async(makeGenerator){
-//   return function () {
-//     const generator = makeGenerator.apply(this, arguments);
-//
-//     function handle(result){
-//       // result => { done: [Boolean], value: [Object] }
-//       if (result.done) return Promise.resolve(result.value);
-//
-//       return Promise.resolve(result.value).then(function (res){
-//         return handle(generator.next(res));
-//       }, function (err){
-//         return handle(generator.throw(err));
-//       });
-//     }
-//
-//     try {
-//       return handle(generator.next());
-//     } catch (ex) {
-//       return Promise.reject(ex);
-//     }
-//   }
-// }
-//
-// async(function* () {
-//   try{
-//     let post2 = yield request.getAsync('http://jsonplaceholder.typicode.com/posts/1');
-//     post2 = JSON.parse(post2.body)
-//     console.log(post2.title);
-//     let user2 = yield request.getAsync('http://jsonplaceholder.typicode.com/users/1');
-//     user2 = JSON.parse(user2.body);
-//     console.log(user2.name);
-//   }catch(e){
-//       console.log(e)
-//   }
-// })();
+function async(makeGenerator){
+  return function () {
+    const generator = makeGenerator.apply(this, arguments);
+
+    function handle(result){
+      // result => { done: [Boolean], value: [Object] }
+      if (result.done) return Promise.resolve(result.value);
+
+      return Promise.resolve(result.value).then(function (res){
+        return handle(generator.next(res));
+      }, function (err){
+        return handle(generator.throw(err));
+      });
+    }
+
+    try {
+      return handle(generator.next());
+    } catch (ex) {
+      return Promise.reject(ex);
+    }
+  }
+}
+
+async(function* () {
+  try{
+    let post = yield request.getAsync('http://jsonplaceholder.typicode.com/posts/1');
+    post = JSON.parse(post.body)
+    let user = yield request.getAsync('http://jsonplaceholder.typicode.com/users/1');
+    user = JSON.parse(user.body);
+    return {
+      status: "OK",
+      user,
+      post,
+    };
+  }catch(e){
+      return {status: "FAILED"};
+  }
+})().then(res => console.log(res));
--- index.js
+++ index.js
@@ -114,13 +114,42 @@ const userIds = [...Array(10)].map((_, i) => i + 1);
 //     Promise.race([late, early]).then(res => console.log(res))
 // })();
 
-// // coroutine with generators
-// Promise.coroutine(function* () {
-//   try{
-//     const post = yield request.getAsync('http://jsonplaceholder.typicode.com/posts/1');
-//     const user = yield request.getAsync('http://jsonplaceholder.typicode.com/users/1');
-//     console.log(post.body, user.body);
-//   }catch(e){
-//     console.log(e)
-//   }
+// coroutine with generators
+Promise.coroutine(function* () {
+  //try{
+    const post = yield request.getAsync('http://jsonplaceholder.typicode.com/posts/1');
+    const user = yield request.getAsync('http://jsonplaceholder.typicode.com/users/1');
+    console.log(post.body, user.body);
+  // }catch(e){
+  //   console.log(e)
+  // }
+})();
+
+// //.reflect
+// (function(){
+//   const post = request.getAsync('http://jsonplaceholder.typicode.com/posts/1');
+//   const user = request.getAsync('http://jsonplaceholder.typicode.com/users/1');
+//
+//   //const promises = [post, user]
+//   // Promise.all(promises.map(function(promise) {
+//   //     return promise.reflect();
+//   // })).each(function(inspection) {
+//   //     if (inspection.isFulfilled()) {
+//   //         console.log("A promise in the array was fulfilled with", JSON.parse(inspection.value().body));
+//   //     } else {
+//   //         console.error("A promise in the array was rejected with", inspection.reason());
+//   //     }
+//   // });
+//
+//   // const object = {post, user}
+//   // Promise.props(Object.keys(object).reduce(function(newObject, key) {
+//   //     newObject[key] = object[key].reflect();
+//   //     return newObject;
+//   // }, {})).then(function(object) {
+//   //     if (object.post.isFulfilled()) {
+//   //         console.log("first was fulfilled with", JSON.parse(object.post.value().body));
+//   //     } else {
+//   //         console.error("first was rejected with", object.first.reason());
+//   //     }
+//   // })
 // })();
